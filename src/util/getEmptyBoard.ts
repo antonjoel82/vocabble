f59b7b0a45c18c8898a86a95a3cb9cab4a0c7c3a
Joel Anton <j.anton.dev@gmail.com>
2023-11-24 13:05:29 -0700

    Add type to util

--- src/util/getEmptyBoard.ts
+++ src/util/getEmptyBoard.ts
@@ -1,4 +1,9 @@
-export const getEmptyBoard = (guessLimit: number, wordLength: number) => {
+import { BoardResults } from "src/components/Board";
+
+export const getEmptyBoard = (
+  guessLimit: number,
+  wordLength: number
+): BoardResults => {
   const board = Array(guessLimit).fill(Array(wordLength).fill({}));
   return board;
 };
